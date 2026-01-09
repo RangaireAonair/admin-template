8c950a17e219352f61f235bbbab1098019a16644
RangaireAonair <newBeePlus@outlook.pt>
2026-01-10 00:47:34 +0800

    fix(bump_version): 修改为更新后的版本号、创建lastet tag

--- scripts/create-release.js
+++ scripts/create-release.js
@@ -1,5 +1,6 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
+import { execSync } from "child_process";
 
 async function run() {
   try {
@@ -9,6 +10,9 @@ async function run() {
 
     if (!token) throw new Error("Missing GITHUB_TOKEN");
     if (!tagName) throw new Error("Missing TAG_NAME");
+
+    execSync("git add package.json");
+    execSync(`git commit -m "chore(release): ${tagName}"`);
 
     const octokit = github.getOctokit(token);
     const { owner, repo } = github.context.repo;
@@ -62,7 +66,11 @@ async function run() {
       prerelease,
       generate_release_notes: true,
     });
+    execSync(`git tag -f latest`);
+    execSync("git push origin HEAD");
+    execSync("git push origin --tags --force");
 
+    console.log("✅ Release completed successfully");
     core.info(`🎉 Release created: ${release.data.html_url}`);
   } catch (err) {
     core.setFailed(`❌ Release failed: ${err.message}`);
